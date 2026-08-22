dcc2b1ffdb4b9ed4a68fad4381b401028c12a923
JasonSucks <70827940+SkidadleSkidoodl@users.noreply.github.com>
2026-08-22 13:49:36 +0800

    Add files via upload

---
+++ Tiled/Chapter1/Tilesets/Dreemur_House/Dreemurr_House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Dreemurr_House" tilewidth="8" tileheight="8" tilecount="100" columns="10">
+ <image source="Dreemurr_House.aseprite" width="80" height="80"/>
+</tileset>
---
+++ Tiled/Chapter1/Tilesets/menu_v2/main_menu_v2_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="main_menu_v2_tiles" tilewidth="8" tileheight="8" tilecount="494" columns="13">
+ <image source="main_menu_v2_tiles.png" width="104" height="184"/>
+</tileset>
